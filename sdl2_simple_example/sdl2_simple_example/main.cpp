b033174050fa1d22200791499dfdb2b2621c763d
Rodrigo Álvarez Moizé <116026830+Rodrigomoize@users.noreply.github.com>
2024-10-07 13:01:15 +0200

    sad

--- sdl2_simple_example/sdl2_simple_example/main.cpp
+++ sdl2_simple_example/sdl2_simple_example/main.cpp
@@ -12,7 +12,7 @@ using ivec2 = glm::ivec2;
 using vec3 = glm::dvec3;
 
 static const ivec2 WINDOW_SIZE(512, 512);
-static const unsigned int FPS = 60;
+static const unsigned int FPS = 60; //dihfs
 static const auto FRAME_DT = 1.0s / FPS;
 
 static void init_openGL() {
